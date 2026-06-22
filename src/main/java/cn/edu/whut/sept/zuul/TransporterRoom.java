2011836a9705c3efba7e715af2e133bee0bce34e
mlkaaaay <3026460971@qq.com>
2026-06-22 22:15:07 +0800

    feat: 实现随机传输房间与魔法饼干属性增益机制
    1.增加具有传输功能的特殊房间，玩家进入后会被随机传输到迷宫中的另一个房间。
    2.在特定房间随机增加magiccookie 物件，并实现 eat cookie 命令，玩家吃掉后永久增长其负重能力上限。

---
+++ src/main/java/cn/edu/whut/sept/zuul/TransporterRoom.java
@@ -0,0 +1,25 @@
+package cn.edu.whut.sept.zuul;
+
+/**
+ * 传输房间类。
+ * 玩家进入此房间后，会被随机传送到地图上的另一个房间。
+ */
+public class TransporterRoom extends Room {
+
+    /**
+     * 构造传输房间。
+     * @param description 描述信息
+     */
+    public TransporterRoom(String description) {
+        super(description);
+    }
+
+    /**
+     * 重写详细描述。
+     * 当玩家看（look）或进入时，给予危险或神秘的提示。
+     */
+    @Override
+    public String getLongDescription() {
+        return "【特殊场景】这是一间充满空间扭曲感的实验室...\n" + super.getLongDescription();
+    }
+}
